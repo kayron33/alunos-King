a8885db7de76b0e4f489629447e77f51c9792a07
kayron33 <129231712+kayron33@users.noreply.github.com>
2024-07-04 17:32:57 -0300

    adicionada varias coisas novas ao sistema

---
+++ inde.js
@@ -0,0 +1,20 @@
+
+var menuItem = document.querySelectorAll('.item-menu')//selecionar icon
+
+//selecionaricone1
+function  selecLink () {
+    menuItem.forEach((item)=>
+    item.classList.remove('ativo')
+)
+this.classList.add('ativo')
+}
+menuItem.forEach((item)=>
+    item.addEventListener('click', selecLink)
+)
+//selecionar icone 1
+var btnExp=document.querySelector('#btn-exp')
+var menuSide=document.querySelector('.menu-lateral')
+
+btnExp.addEventListener('click',function(){
+    menuSide.classList.toggle('expandir')
+})
