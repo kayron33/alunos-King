ff6d66a8f556d901551a21da70761a2e16b840be
kayron33 <129231712+kayron33@users.noreply.github.com>
2024-07-09 16:28:24 -0300

    opa

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>KING</title>
+</head>
+<body>
+<header>
+    <div class="logo">
+        <img src="img/KING_LOGO-removebg-preview.png" alt="logo da empresa">
+    </div>
+    
+</header>
+
+    
